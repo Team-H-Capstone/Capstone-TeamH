1dcfbcab4a62a14ad14417a188bb84ef14ce8573
Eva Li <evali3101@gmail.com>
2023-01-25 17:41:49 -0500

    created about page w/ css

---
+++ src/components/About.js
@@ -0,0 +1,115 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div className="w-full h-screen">
+      <section 
+      className="flex flex-col justify-center items-center w-full h-2/4 bg-[#dad7cd] text-[#344e41]"
+      >
+        <h1 className="text-7xl font-bold tracking-wide">
+            About Us
+        </h1>
+        <h2 className="text-lg italic pt-10">
+          "Vulnerability sounds like truth and feels like courage. Truth and
+          courage aren't always comfortable, but they're never weakness." —
+          Brené Brown
+        </h2>
+      </section>
+      <section className="flex flex-row justify-center items-center w-full py-12">
+        <div className="flex flex-col pl-20" >
+          <h2
+          className="text-4xl font-bold pb-4"
+          >
+            Mental Health Awareness
+          </h2>
+          <h3
+          className="text-lg"
+          >
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Nec nam
+            aliquam sem et. Tincidunt id aliquet risus feugiat in. Aliquet nec
+            ullamcorper sit amet. Etiam non quam lacus suspendisse faucibus
+            interdum. Bibendum ut tristique et egestas.
+            <br></br>
+            <br></br>
+            Ut faucibus pulvinar elementum integer. Odio aenean sed adipiscing
+            diam donec adipiscing tristique. Volutpat commodo sed egestas
+            egestas fringilla phasellus. Sagittis vitae et leo duis. Nibh tortor
+            id aliquet lectus proin nibh nisl. Tellus cras adipiscing enim eu
+            turpis. Diam quis enim lobortis scelerisque.Tincidunt id aliquet risus feugiat in. Aliquet nec ullamcorper sit amet.
+          </h3>
+        </div>
+        <img
+          width="54%"
+          alt="aboutPic"
+          src="https://syncromsp.com/wp-content/uploads/2022/08/how-to-encourage-mental-health-awareness-at-your-msp.png
+        "
+        />
+      </section>
+      <section className="flex flex-col justify-center items-center w-full">
+        <h2 className="text-3xl font-bold"
+        >Contact Us
+        </h2>
+        <div className="flex flex-row pb-7 pt-1">
+          <div className="flex flex-col justify-center items-center px-5">
+            <img
+              width="200"
+              alt="creator"
+              src="https://media.istockphoto.com/id/1300845620/vector/user-icon-flat-isolated-on-white-background-user-symbol-vector-illustration.jpg?s=612x612&w=0&k=20&c=yBeyba0hUkh14_jgv1OKqIH0CCSWU_4ckRkAoy2p73o="
+            />
+            <div className="space-x-3 text-lg font-medium" >
+                <a className="hover:text-[#e63946]" href="http://www.linkedin.com">LinkedIn</a>
+                <a className="hover:text-[#e63946]" href="http://www.github.com">GitHub</a>
+            </div>
+          </div>
+          <div className="flex flex-col justify-center items-center px-5">
+            <img
+              width="200"
+              alt="creator"
+              src="https://media.istockphoto.com/id/1300845620/vector/user-icon-flat-isolated-on-white-background-user-symbol-vector-illustration.jpg?s=612x612&w=0&k=20&c=yBeyba0hUkh14_jgv1OKqIH0CCSWU_4ckRkAoy2p73o="
+            />
+            <div className="space-x-3 text-lg font-medium" >
+                <a className="hover:text-[#e63946]" href="http://www.linkedin.com">LinkedIn</a>
+                <a className="hover:text-[#e63946]" href="http://www.github.com">GitHub</a>
+            </div>
+          </div>
+          <div className="flex flex-col justify-center items-center px-5">
+            <img
+              width="200"
+              alt="creator"
+              src="https://media.istockphoto.com/id/1300845620/vector/user-icon-flat-isolated-on-white-background-user-symbol-vector-illustration.jpg?s=612x612&w=0&k=20&c=yBeyba0hUkh14_jgv1OKqIH0CCSWU_4ckRkAoy2p73o="
+            />
+            <div className="space-x-3 text-lg font-medium" >
+                <a className="hover:text-[#e63946]" href="http://www.linkedin.com">LinkedIn</a>
+                <a className="hover:text-[#e63946]" href="http://www.github.com">GitHub</a>
+            </div>
+          </div>
+          <div className="flex flex-col justify-center items-center px-5">
+            <img
+              width="200"
+              alt="creator"
+              src="https://media.istockphoto.com/id/1300845620/vector/user-icon-flat-isolated-on-white-background-user-symbol-vector-illustration.jpg?s=612x612&w=0&k=20&c=yBeyba0hUkh14_jgv1OKqIH0CCSWU_4ckRkAoy2p73o="
+            />
+            <div className="space-x-3 text-lg font-medium" >
+                <a className="hover:text-[#e63946]" href="http://www.linkedin.com">LinkedIn</a>
+                <a className="hover:text-[#e63946]" href="http://www.github.com">GitHub</a>
+            </div>
+          </div>
+          <div className="flex flex-col justify-center items-center px-5">
+            <img
+              width="200"
+              alt="creator"
+              src="https://media.istockphoto.com/id/1300845620/vector/user-icon-flat-isolated-on-white-background-user-symbol-vector-illustration.jpg?s=612x612&w=0&k=20&c=yBeyba0hUkh14_jgv1OKqIH0CCSWU_4ckRkAoy2p73o="
+            />
+            <div className="space-x-3 text-lg font-medium" >
+                <a className="hover:text-[#e63946]" href="http://www.linkedin.com">LinkedIn</a>
+                <a className="hover:text-[#e63946]" href="http://www.github.com">GitHub</a>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default About;
