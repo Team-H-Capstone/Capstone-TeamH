39a84473e397578717d94ad8a10e408142b16447
Justin Signo <justinsigno@yahoo.com>
2023-01-25 23:34:07 -0500

    login is almost working

--- src/components/Hello.js
+++ src/components/Hello.js
@@ -5,6 +5,7 @@ import "aos/dist/aos.css";
 import { HiArrowNarrowRight } from "react-icons/hi";
 import { Link } from "react-scroll";
 import Login from "./Login";
+import Register from "./Register"
 
 const Hello = () => {
   useEffect(() => {
@@ -67,16 +68,8 @@ const Hello = () => {
         >
           *OUR WEBSITE NAME*
         </h1>
-        <div className="flex flex-row">
-          <button className="text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-orange-600 hover:border-orange-600">
-            Sign Up
-          </button>
-
-          <button className="text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-orange-600 hover:border-orange-600">
-            Sign In
-          </button>
-        </div>
         <Login />
+        <Register />
       </section>
     </div>
   );
