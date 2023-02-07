755c3c37aef9c98dbebbd31338fe68c2a9a596e3
Tien La <tiencla@outlook.com>
2023-02-07 18:03:19 -0500

    bird parallax done. WIP

--- src/components/Hello.js
+++ src/components/Hello.js
@@ -1,41 +1,84 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useRef } from 'react';
 
-import AOS from "aos";
-import "aos/dist/aos.css";
-import { HiArrowNarrowRight } from "react-icons/hi";
-import Linky from "react-scroll";
-import Login from "./Login";
-import Register from "./Register";
-import { auth } from "../firebase/firebase-config";
-import { useAuthState } from "react-firebase-hooks/auth";
-import { Link } from "react-router-dom";
-import Chart from "./Chart";
-import Quiz from "./Quiz";
-import AdultChart from "../AdultChart";
-import MusicPlayer from "./MusicPlayer/MusicPlayer";
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+import { HiArrowNarrowRight } from 'react-icons/hi';
+import Linky from 'react-scroll';
+import Login from './Login';
+import Register from './Register';
+import { auth } from '../firebase/firebase-config';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { Link } from 'react-router-dom';
+import Chart from './Chart';
+import Quiz from './Quiz';
+import AdultChart from '../AdultChart';
+import { documentId } from 'firebase/firestore';
+
+const bird1Img = 'img/bird1.png';
+const bird2Img = 'img/bird2.png';
+const forestImg = 'img/forest.png';
+const rocksImg = 'img/rocks.png';
+const waterImg = 'img/water.png';
 
 const Home = () => {
+  const text = document.getElementById('text');
+  const bird1 = document.getElementById('bird1');
+  const bird2 = document.getElementById('bird2');
+  const forest = document.getElementById('forest')
+  const rocks = document.getElementById('rocks');
+  const water = document.getElementById('water');
+
+  // const textRef = useRef(null);
+  // const bird1Ref = useRef(null);
+  // const bird2Ref = useRef(null);
+  // const forestRef = useRef(null);
+  // const rocksRef = useRef(null);
+  // const waterRef = useRef(null);
+
+   window.addEventListener('scroll', function() {
+      let value = window.scrollY;
+
+      text.style.top = 50 + value * -0.5 + '%';
+      bird1.style.top = value * -1.5 + 'px';
+      bird1.style.left = value * 2 + 'px';
+      bird2.style.top = value * -1.5 + 'px';
+      bird2.style.left = value * -5 + 'px';
+
+      // textRef.current.style.top = 50 + value * -0.5 + '%';
+      // bird1Ref.current.style.top = value * -1.5 + 'px';
+      // bird1Ref.current.style.left = value * 2 + 'px';
+      // bird2Ref.current.style.top = value * -1.5 + 'px';
+      // bird2Ref.current.style.left = value * -5 + 'px';
+    })
+
+
+
   useEffect(() => {
     AOS.init();
+   
   }, []);
 
   const [user] = useAuthState(auth);
-  console.log(user);
+  // console.log(user);
 
   return (
     <div className="w-full h-screen">
-      <section className="flex flex-col justify-center items-center w-full h-full bg-[#1e3a8a] text-white" style={{backgroundImage: "radial-gradient(#2b53c4, #1e3987, #325edb)"}}>
-        <h1
-          className="flex justify-center items-center text-9xl font-bold text-center"
-          style={{fontSize: "5vw"}}
-          data-aos="fade-zoom-in"
-          data-aos-easing="ease-in-back"
-          data-aos-delay="500"
-          data-aos-offset="0"
-        >
-          Hello {user ? user.displayName : ""}!
-        </h1>
-        <h1
+      <section className="flex flex-col justify-center items-center w-full h-full relative bg-[#1e3a8a] text-white" style={{ backgroundImage: 'radial-gradient(#2b53c4, #1e3987, #325edb)' }}>
+        <h2 className="flex justify-center items-center text-9xl font-bold text-center" id='text' style={{ fontSize: '5vw' }} data-aos="fade-zoom-in" data-aos-easing="ease-in-back" data-aos-delay="500" data-aos-offset="0">
+          <span className="tracking-wide leading-none ">
+            {' '}
+            Hello <br />
+            {user ? user.displayName : ''}!
+          </span>
+          {/* <br/>{user ? user.displayName : ""}! */}
+        </h2>
+        <img src={bird1Img} id='bird1' alt='bird1'/>
+        <img src={bird2Img} id='bird2' alt='bird2'/>
+        <img src={forestImg} id='forest' alt='forest'/>
+        <button className='' id='home_btn'>Explore</button>
+        <img src={rocksImg} id='rocks' alt='rocks'/>
+        <img src={waterImg} id='water' alt='water'/>
+        {/* <h1
           className="flex justify-center items-center text-9xl font-bold text-center"
           data-aos="fade-zoom-in"
           data-aos-easing="ease-in-back"
@@ -44,171 +87,114 @@ const Home = () => {
           style={{fontSize: "5vw"}}
         >
           How are you doing today?
-        </h1>
+        </h1> */}
       </section>
+      {/* <div id=''></div> */}
       {/* <section
         name="question"
         className="flex flex-col justify-center items-center w-full h-full bg-[#1e3a8a] text-white" style={{backgroundImage: "radial-gradient(#2b53c4, #1e3987, #325edb)"}}
       >
         
       </section> */}
-      <section name="question" className="flex flex-col justify-center items-center w-full h-full bg-[#1e3a8a] text-white" style={{backgroundImage: "radial-gradient(#2b53c4, #1e3987, #325edb)"}}>
-        <h1
-          className="flex justify-center items-center text-9xl font-bold text-center"
-          data-aos="fade-zoom-in"
-          data-aos-easing="ease-in-back"
-          data-aos-delay="500"
-          data-aos-offset="0"
-          style={{fontSize: "5vw"}}
-        >
+      <section name="question" className="flex flex-col justify-center items-center w-full h-full bg-[#1e3a8a] text-white" style={{ backgroundImage: 'radial-gradient(#2b53c4, #1e3987, #325edb)' }}>
+        <h1 className="flex justify-center items-center text-9xl font-bold text-center" data-aos="fade-zoom-in" data-aos-easing="ease-in-back" data-aos-delay="500" data-aos-offset="0" style={{ fontSize: '5vw' }}>
           Welcome to
         </h1>
-        <h1
-          data-aos="fade-zoom-in"
-          data-aos-easing="ease-in-back"
-          data-aos-delay="1100"
-          data-aos-offset="0"
-          className="title"
-        >
+        <h1 data-aos="fade-zoom-in" data-aos-easing="ease-in-back" data-aos-delay="1100" data-aos-offset="0" className="title">
           BE MINDFUL
         </h1>
       </section>
-      <section className="flex flex-col justify-center items-center w-full h-full bg-[#1e3a8a] text-white" style={{backgroundImage: "radial-gradient(#2b53c4, #1e3987, #325edb)"}}>
-        <h1
-          className="flex justify-center items-center text-5xl font-bold text-center w-50 h-400"
-          data-aos="fade-zoom-in"
-          data-aos-easing="ease-in-back"
-          data-aos-delay="1000"
-          data-aos-offset="0"
-          style={{fontSize: "2vw"}}
-        >
+      <section className="flex flex-col justify-center items-center w-full h-full bg-[#1e3a8a] text-white" style={{ backgroundImage: 'radial-gradient(#2b53c4, #1e3987, #325edb)' }}>
+        <h1 className="flex justify-center items-center text-5xl font-bold text-center w-50 h-400" data-aos="fade-zoom-in" data-aos-easing="ease-in-back" data-aos-delay="1000" data-aos-offset="0" style={{ fontSize: '2vw' }}>
           Did you know mental health affects hundreds of millions of people around the world?
         </h1>
         <br></br>
         <br></br>
         <Chart />
       </section>
-      <section
-        name="question"
-        className="flex flex-col justify-center items-center w-full h-full bg-[#1e3a8a] text-white"
-        style={{backgroundImage: "radial-gradient(#2b53c4, #1e3987, #325edb)"}}
-      >
+      <section name="question" className="flex flex-col justify-center items-center w-full h-full bg-[#1e3a8a] text-white" style={{ backgroundImage: 'radial-gradient(#2b53c4, #1e3987, #325edb)' }}>
         <br></br>
-        <h1 className="flex justify-center text-center items-center text-5xl font-bold"
-          data-aos="fade-zoom-in"
-          data-aos-easing="ease-in-back"
-          data-aos-delay="500"
-          data-aos-offset="0"
-          style={{fontSize: "2vw"}}
-          >
+        <h1 className="flex justify-center text-center items-center text-5xl font-bold" data-aos="fade-zoom-in" data-aos-easing="ease-in-back" data-aos-delay="500" data-aos-offset="0" style={{ fontSize: '2vw' }}>
           Statistics of mental health prevalence in the United States of America
-          </h1>
-          <br></br>
-          <br></br>
-          <AdultChart />
-      </section>
-      <section
-        name="question"
-        className="flex flex-col justify-center items-center w-full h-full bg-[#1e3a8a] text-white"
-        style={{backgroundImage: "radial-gradient(#2b53c4, #1e3987, #325edb)"}}
-      >
-        <br></br>
-        <h1 className="flex justify-center text-center items-center text-5xl font-bold"
-          data-aos="fade-zoom-in"
-          data-aos-easing="ease-in-back"
-          data-aos-delay="500"
-          data-aos-offset="0"
-          style={{fontSize: "2vw"}}
-          >
-          Want to learn more about Mental Health? Take the quiz!
-          </h1>
-          <br></br>
-          <Quiz />
-      </section>
-      <section
-        name="question"
-        className="flex flex-col justify-center items-center w-full h-full bg-[#1e3a8a] text-white"
-        style={{backgroundImage: "repeating-radial-gradient(#2b53c4, #1e3987 10%, #325edb 15%)"}}
-      >
-        <h1
-          className="flex justify-center items-center text-9xl font-bold text-center"
-          data-aos="fade-zoom-in"
-          data-aos-easing="ease-in-back"
-          data-aos-delay="500"
-          data-aos-offset="0"
-          style={{fontSize: "5vw"}}
-        >
-        Enter Our Mental Spa
         </h1>
-        <h1
-          style={{ fontStyle: "italic", marginTop: 50, fontSize: "2vw" }}
-          className="flex justify-center items-center text-5xl font-bold"
-          data-aos="fade-zoom-in"
-          data-aos-easing="ease-in-back"
-          data-aos-delay="1000"
-          data-aos-offset="0"
-        >
+        <br></br>
+        <br></br>
+        <AdultChart />
+      </section>
+      <section name="question" className="flex flex-col justify-center items-center w-full h-full bg-[#1e3a8a] text-white" style={{ backgroundImage: 'radial-gradient(#2b53c4, #1e3987, #325edb)' }}>
+        <br></br>
+        <h1 className="flex justify-center text-center items-center text-5xl font-bold" data-aos="fade-zoom-in" data-aos-easing="ease-in-back" data-aos-delay="500" data-aos-offset="0" style={{ fontSize: '2vw' }}>
+          Want to learn more about Mental Health? Take the quiz!
+        </h1>
+        <br></br>
+        <Quiz />
+      </section>
+      <section name="question" className="flex flex-col justify-center items-center w-full h-full bg-[#1e3a8a] text-white" style={{ backgroundImage: 'repeating-radial-gradient(#2b53c4, #1e3987 10%, #325edb 15%)' }}>
+        <h1 className="flex justify-center items-center text-9xl font-bold text-center" data-aos="fade-zoom-in" data-aos-easing="ease-in-back" data-aos-delay="500" data-aos-offset="0" style={{ fontSize: '5vw' }}>
+          Enter Our Mental Spa
+        </h1>
+        <h1 style={{ fontStyle: 'italic', marginTop: 50, fontSize: '2vw' }} className="flex justify-center items-center text-5xl font-bold" data-aos="fade-zoom-in" data-aos-easing="ease-in-back" data-aos-delay="1000" data-aos-offset="0">
           Interactive Meditation Rooms
         </h1>
         <div
           style={{
-            width: "100vw",
+            width: '100vw',
             marginTop: 100,
-            display: "flex",
-            flexDirection: "row",
-            justifyContent: "center",
+            display: 'flex',
+            flexDirection: 'row',
+            justifyContent: 'center',
           }}
         >
-          <Link to='/music'>
-          <div
-            style={{
-              display: "flex",
-              width: "25vw",
-              height: "25vh",
-              alignItems: "right",
-              border: "10px solid white",
-              backgroundImage: `url("/img/meditation.jpeg")`,
-              backgroundPosition: "center",
-              backgroundSize:"cover",
-              borderRadius: 50,
-              marginRight: 25
-            }}
-          >
-            <h1
+          <Link to="/music">
+            <div
               style={{
-                display: "flex",
-                alignItems: "center",
-                textAlign: "center",
-                justifyContent: "center",
-                width: "100%",
-                fontSize: "3vw"
+                display: 'flex',
+                width: '25vw',
+                height: '25vh',
+                alignItems: 'right',
+                border: '10px solid white',
+                backgroundImage: `url("/img/meditation.jpeg")`,
+                backgroundPosition: 'center',
+                backgroundSize: 'cover',
+                borderRadius: 50,
+                marginRight: 25,
               }}
             >
-              Music & Videos
-            </h1>
-          </div>
+              <h1
+                style={{
+                  display: 'flex',
+                  alignItems: 'center',
+                  textAlign: 'center',
+                  justifyContent: 'center',
+                  width: '100%',
+                  fontSize: '3vw',
+                }}
+              >
+                Music & Videos
+              </h1>
+            </div>
           </Link>
 
           <Link to="/memoryGame">
             <div
               style={{
-                display: "flex",
-                width: "25vw",
-                height: "25vh",
-                alignItems: "right",
-                border: "10px solid white",
+                display: 'flex',
+                width: '25vw',
+                height: '25vh',
+                alignItems: 'right',
+                border: '10px solid white',
                 backgroundImage: `url("/img/gameBackground.jpeg")`,
-                backgroundPosition: "center",
-                borderRadius: 50
+                backgroundPosition: 'center',
+                borderRadius: 50,
               }}
             >
               <h1
                 style={{
-                  display: "flex",
-                  alignItems: "center",
-                  justifyContent: "center",
-                  width: "100%",
-                  fontSize: "3vw"
+                  display: 'flex',
+                  alignItems: 'center',
+                  justifyContent: 'center',
+                  width: '100%',
+                  fontSize: '3vw',
                 }}
               >
                 Games
